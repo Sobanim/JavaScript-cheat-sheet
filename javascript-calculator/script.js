6e6f96759b5472b6e836b0040592d9cc0f52c6e9
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-07-03 11:16:07 +0200

    Calculator finished
    Calculator works fine, but on the futute I want add enter with keyboard

--- javascript-calculator/script.js
+++ javascript-calculator/script.js
@@ -1,3 +1,17 @@
 function insert(num){
     document.getElementById("textfield").value += num;
 }
+function clean (){
+    document.getElementById("textfield").value = "";
+}
+function del(){
+    var exp = document.getElementById("textfield").value;
+    document.getElementById("textfield").value = exp.substring(0, exp.length-1);
+}
+
+function equal() {
+    var exp = document.getElementById("textfield").value;
+    if(exp){
+        document.getElementById("textfield").value = eval(exp);
+    }
+}
