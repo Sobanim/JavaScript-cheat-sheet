d68f05612fcb5f67fec6c36e55132346b36b04fd
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-07-04 19:16:20 +0200

    Working with up number
    Not workind for numpad numbers

--- javascript-calculator/script.js
+++ javascript-calculator/script.js
@@ -16,6 +16,42 @@ function equal() {
     }
 }
 
-document.querySelector('#textfield').onkeypress = function (event) {
-    console.log('key' + event.key);
+document.querySelector('body').onkeypress = function (event) {
+    console.log(event);
+
+    switch (event.code){
+        case "Digit1":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 1;
+            break;
+        case "Digit2":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 2;
+            break;
+        case "Digit3":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 3;
+            break;
+        case "Digit4":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 4;
+            break;
+        case "Digit5":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 5;
+            break;
+        case "Digit6":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 6;
+            break;
+        case "Digit7":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 7;
+            break;
+        case "Digit8":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 8;
+            break;
+        case "Digit9":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 9;
+            break;
+        case "Digit0":
+            document.getElementById("textfield").value = document.getElementById("textfield").value + 0;
+            break;
+        case "Equal":
+            equal();
+            break;
+    }
 }
